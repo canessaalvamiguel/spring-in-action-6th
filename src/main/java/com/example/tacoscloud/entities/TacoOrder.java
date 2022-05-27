8cc7f4f562fd358235de58a5bfeb035abd14e96a
Miguel Canessa <canessaalvamiguel@gmail.com>
2022-05-26 23:23:01 -0500

    Adding missing lombok data anotation

--- src/main/java/com/example/tacoscloud/entities/TacoOrder.java
+++ src/main/java/com/example/tacoscloud/entities/TacoOrder.java
@@ -1,14 +1,17 @@
 package com.example.tacoscloud.entities;
 
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
 public class TacoOrder {
     private String deliveryName;
     private String deliveryStreet;
     private String deliveryCity;
     private String deliveryState;
-    private String getDeliveryZip;
+    private String DeliveryZip;
     private String ccNumber;
     private String ccExpiration;
     private String ccCVV;
