e0b2319a144a021b36e864f1481d579ef787d6b3
Miguel Canessa <canessaalvamiguel@gmail.com>
2022-05-27 18:20:52 -0500

    Implementing repository integration

---
+++ src/main/java/com/example/tacoscloud/repository/IngredientRepository.java
@@ -0,0 +1,11 @@
+package com.example.tacoscloud.repository;
+
+import com.example.tacoscloud.entities.Ingredient;
+
+import java.util.Optional;
+
+public interface IngredientRepository {
+    Iterable<Ingredient> findAll();
+    Optional<Ingredient> findById(String id);
+    Ingredient save(Ingredient ingredient);
+}
