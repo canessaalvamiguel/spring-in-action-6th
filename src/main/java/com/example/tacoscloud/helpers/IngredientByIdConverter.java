db5d96d6bb234e5ffa0fc5072d5509b8ec29b28a
Miguel Canessa <canessaalvamiguel@gmail.com>
2022-05-31 11:53:49 -0500

    Adding missing converter

---
+++ src/main/java/com/example/tacoscloud/helpers/IngredientByIdConverter.java
@@ -0,0 +1,22 @@
+package com.example.tacoscloud.helpers;
+
+import com.example.tacoscloud.entities.Ingredient;
+import com.example.tacoscloud.repository.IngredientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IngredientByIdConverter implements Converter<String, Ingredient> {
+
+    private IngredientRepository ingredientRepo;
+    @Autowired
+    public IngredientByIdConverter(IngredientRepository ingredientRepo) {
+        this.ingredientRepo = ingredientRepo;
+    }
+
+    @Override
+    public Ingredient convert(String id) {
+        return ingredientRepo.findById(id).orElse(null);
+    }
+}
